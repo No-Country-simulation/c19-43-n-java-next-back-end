7a9833c0fc4a2e213577fb9055ffad8aee622541
arimakey <127151745+arimakey@users.noreply.github.com>
2024-08-01 21:59:35 -0500

    Probando el edge runtime

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 'use server'
 
+export const runtime = 'edge'
+
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@radix-ui/react-navigation-menu";
 
 import { navbar_list } from "@/config/config";
