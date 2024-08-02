45b4faac633ab137ca312635e50c08c8387814ec
arimakey <127151745+arimakey@users.noreply.github.com>
2024-08-01 23:46:48 -0500

    feat: Se añadio la pagina doctors

---
+++ src/app/doctors/page.tsx
@@ -0,0 +1,20 @@
+export const runtime = 'edge'
+
+import { userGetDoctors } from '@/api/data'
+import { RegDoctor } from '@/api/dtypes'
+import ListDoctor from '@/components/ListDoctor'
+import DoctList from '@/components/user/doctors/DoctList'
+import TestButton from '@/components/user/TestButton'
+
+export default async function page() {
+  
+  let {data} = await userGetDoctors()
+
+  return (
+    <div className="">
+      <h1>Listado de doctores</h1>
+      <DoctList params={{lstdocts:data}} />
+      <TestButton data={data} />
+    </div>
+  )
+}
