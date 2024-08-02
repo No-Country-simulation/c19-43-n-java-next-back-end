bb740391685552d39d6ff66953fa35a2a693b494
arimakey <127151745+arimakey@users.noreply.github.com>
2024-08-01 20:46:50 -0500

    fix: Se agrego key a un iterador

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 'use server'
-export const runtime = 'edge';
 import ProfileForm from '@/components/ProfileForm'
 import { getUser } from '@/api/auth'
 import { cookies } from 'next/headers';
@@ -9,7 +8,7 @@ export default async function ProfilePage() {
   const token = cookieStore.get('token');
 
   const user_data = await getUser(token?.value);
-  console.log(user_data)
+
   return (
     <div>
       <h1>Editar Perfil</h1>
