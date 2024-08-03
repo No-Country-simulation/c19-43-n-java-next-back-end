149540441d704a377a4077b2a486446d00353f8a
Jose Angel Suarez <127800781+angeljs95@users.noreply.github.com>
2024-08-03 18:19:20 -0300

    Delete telemedicinaBack directory

--- telemedicinaBack/src/main/java/com/NoCountry/telemedicinaBack/Enum/DiasDeLaSemana.java
+++
@@ -1,7 +0,0 @@
-package com.NoCountry.telemedicinaBack.Enum;
-
-public enum DiasDeLaSemana {
-
-    LUNES, MARTES, MIERCOLES,JUEVES,VIERNES,SABADO,DOMINGO
-
-}
--- telemedicinaBack/src/main/java/com/NoCountry/telemedicinaBack/Exceptions/MiException.java
+++
@@ -1,9 +0,0 @@
-package com.NoCountry.telemedicinaBack.Exceptions;
-
-public class MiException extends Exception{
-
-    public MiException(String msg){
-        super(msg);
-    }
-
-}
